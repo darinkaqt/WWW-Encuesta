64ea0ef8594b2417dfc00e4fe2595e90e9b4bffd
Darinka <darinka.quinones@sansano.usm.cl>
2021-08-05 19:44:02 -0400

    primer commit

---
+++ src/requests/encuestas.js
@@ -0,0 +1,14 @@
+export default {
+    encuestas: [
+        {   
+            id: 1,
+            titulo: 'Comida favorita',
+            descripcion: "Lasaña"
+        },
+        {   
+            id: 2,
+            titulo: 'Animal favorito',
+            descripcion: "Pingüino"
+        }
+    ]
+}
