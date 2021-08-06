0ac1684d50dad64806e96008b53a05752d071cc5
raulcruztorres <raul.cruz@sansano.usm.cl>
2021-08-05 23:01:41 -0400

    listGroup

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Descargar
 
-npm install @material-ui/core
-npm install @material-ui/icons
+- npm install @material-ui/core
+- npm install @material-ui/icons
+- npm install react-bootstrap@next bootstrap@5.0.2
--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 import Box from '@material-ui/core/Box';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Form, Button} from 'react-bootstrap'
+import {Form, Button, ListGroup} from 'react-bootstrap'
+import data from './requests/encuestas';
+/* import {Card, CardTitle, CardMedia, CardText} from 'material-ui/core'; */
 
 
 class App extends Component{
@@ -11,9 +13,43 @@ class App extends Component{
 		super(props);
 		this.state = {
 			encuestas: [],
-			cargado: false // variable de estado
+			cargado: false, // variable de estado
+			idEncuesta: null
 		};
 	}
+
+	//Métodos
+	encuestas(){
+		let encuestas = data.encuestas.map(enc=>{
+			return(
+			<div>
+				<ListGroup>
+					<ListGroup.Item>{enc.id}</ListGroup.Item>
+					<ListGroup.Item>{enc.titulo}</ListGroup.Item>
+					<ListGroup.Item>{enc.descripcion}</ListGroup.Item>
+				</ListGroup>
+			</div>
+			)
+		});
+		return encuestas;
+	}
+/* 	// Guarda el id escrito en el campo de texto
+	handleEncuesta(e){
+        if(idEncuesta !== "") this.setState({idEncuesta: e.target.value});
+    }
+	// Busca y verifica el id dentro de la lista de de encuestas
+	handleFind(e){
+		let aux = encuestas.filter(x => x.id == e.target.idEncuesta).map(y =>{
+			return(
+				<div>
+					<p>{y.titulo}</p>
+					<p>{y.descripcion}</p>
+				</div>	
+			);
+		})
+		return aux;
+	} */
+
 
 	render(){
 		return (
@@ -29,11 +65,18 @@ class App extends Component{
 							</div>
 							<Form.Group className="mb-3" controlId="formBasicEmail">
 								<Form.Label>ID</Form.Label>
-								<Form.Control type="text" placeholder="ID de encuesta a buscar" />
+								<Form.Control type="text" /* onChange={this.handleEncuesta} */ placeholder="ID de encuesta a buscar" />
 							</Form.Group>
-							<Button variant="dark" type="submit">
+							<Button /* id={idEncuesta} onClick={this.handleFind} */ variant="outline-dark" type="submit">
 								Buscar
 							</Button>
+							<br></br>
+							<br></br>
+							<br></br>
+							<div className="row">
+								<h5>Encuestas</h5>
+                                {this.encuestas()}
+                            </div>
 						</fieldset>
 					</Form>
 				</div>
